c5e21198108d688dfc97aa6d96978a72c10c705a
zito <shenzt@iccas.ac.cn>
2020-01-10 21:58:57 +0800

    backup20200110

--- double_sfg/NC_shift.f90
+++ double_sfg/NC_shift.f90
@@ -1,24 +1,24 @@
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !                                                                       !
 !     NC_shift: calculate the shifts of Normal Coordinates              !
+!               For the displaced oscillor approximation                ! 
 !                                                                       !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       MODULE NC_shift
-      USE contants
-      USE atom_mass
       IMPLICIT NONE
 !
       CONTAINS
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      SUBROUTINE Delta_Q(natom,freq,dq)
+      SUBROUTINE Delta_Q(natom,dq)
       IMPLICIT NONE
       INTEGER,INTENT(IN) :: natom
-      REAL(KIND=8),DIMENSION(natom*3-6),INTENT(OUT) :: freq,dq
-      INTEGER :: i,j,itemp
+      REAL(KIND=8),DIMENSION(natom*3-6),INTENT(OUT) :: dq
+      INTEGER :: i,j,k,itemp,lblock
       INTEGER :: mass(natom)
-      REAL(KIND=8),DIMENSION(3,natom*3-6) :: xcoor,xcoorp,dx
-      REAL(KIND=8),DIMENSION(natom*3-6) :: dqp,redmass,f_norm
-      REAL(KIND=8),DIMENSION(natom*3-6,natom*3) :: lmatrix,lmw
+      REAL(KIND=8),DIMENSION(3,natom) :: xcoor,xcoorp
+      REAL(KIND=8),DIMENSION(natom*3) :: dx
+      REAL(KIND=8),DIMENSION(natom*3-6) :: redmass
+      REAL(KIND=8),DIMENSION(natom*3,natom*3-6) :: lmat
       CHARACTER(100) :: filename,buffer
       LOGICAL :: ok
 !
@@ -31,80 +31,87 @@
 ! read reduced mass
         READ(79,*) buffer
         DO i=1,natom*3-6
-          READ(79,*) redmass(i) 
+          READ(79,*) redmass(i)   ! in AMU 
         END DO
 ! read geometry of S0 state in Cartesian coordinates
         READ(79,*) buffer
         DO i=1,natom
-          READ(79,*) itemp,itemp,xcoor(1,i),xcoor(2,i),xcoor(3,i)
+          READ(79,*) itemp,itemp,xcoor(1:3,i)
         END DO
 ! read geometry of S1 state in Cartesian coordinates
         READ(79,*) buffer
         DO i=1,natom
-          READ(79,*) itemp,itemp,xcoorp(1,i),xcoorp(2,i),xcoorp(3,i)
+          READ(79,*) itemp,itemp,xcoorp(1:3,i)
         END DO
-      ELSE
-        WRITE(*,*) "File ",'"',TRIM(filename),'"', " doesn't exist."
-      END IF
-      CLOSE(79)
-!
-! calculate displacement between S0 and S1 states
-      dx(:,:)=xcoorp(:,:)-xcoor(:,:)
-!
-! the normal modes calculated by Gaussian, i.e., l matrix are read in
-      filename="acetone.norm"
-      INQUIRE(file=filename,exist=ok)
-      IF(ok) THEN
-        OPEN(unit=79,file=filename,form='formatted',status='old')
-        i=1;io=0
-        DO WHILE((i*5)<=(natom*3-6))
-          DO WHILE(.TRUE.)
-            READ(unit=79,fmt='(A100)',iostat=io) buffer
-            buffer=ADJUSTL(buffer)
-            IF(buffer(1:11)==nu_flag) THEN
-              BACKSPACE(79)
-              READ(79,*) segment,segment,freq((i*5-4):i*5)         ! read frequency
-              READ(79,*) segment,segment,segment,redmass((i*5-4):i*5) ! read reduced mass
-              DO j=1,3
-                READ(79,'(A100)') buffer
-              END DO
-              DO j=1,natom*3
-                READ(unit=79,fmt=*,iostat=io) segment,segment,segment, &
-     &          lmatrix((i*5-4):i*5,j)
-              END DO
-            IF(.TRUE.) EXIT
-            END IF
+! read l matrix of S0 state in Gaussian form
+        READ(79,*) buffer
+        IF(MOD(natom*3-6,5).NE.0) THEN
+          lblock=(natom*3-6)/5+1
+          DO i=1,lblock-1
+            DO k=1,8 
+              READ(79,*) buffer
+            END DO
+            DO j=1,natom*3
+              READ(79,*) itemp,itemp,itemp,lmat(j,(i*5-4):(i*5))
+            END DO
           END DO
-          i=i+1
-        END DO
-        IF((i-1)*5<(natom*3-6)) THEN
-          READ(unit=79,fmt='(A100)',iostat=io) buffer
-          READ(unit=79,fmt='(A100)',iostat=io) buffer
-          READ(79,*) segment,segment,freq(((i-1)*5+1):(natom*3-6)) ! read frequency
-          READ(79,*) segment,segment,segment, &
-     &               redmass(((i-1)*5+1):(natom*3-6))              ! read reduced mass
-          DO j=1,3
-            READ(79,'(A100)') buffer
+          DO k=1,8   ! read the last block of l matrix
+            READ(79,*) buffer
           END DO
-          DO j=1,natom*3
-            READ(unit=79,fmt=*,iostat=io) segment,segment,segment, &
-     &      lmatrix(((i-1)*5+1):(natom*3-6),j)
+          IF(MOD(natom*3-6,5)==1) THEN
+            DO j=1,natom*3
+              READ(79,*) itemp,itemp,itemp,lmat(j,natom*3-6)
+            ENDDO
+          ELSE IF(MOD(natom*3-6,5)==2) THEN
+            DO j=1,natom*3
+              READ(79,*) itemp,itemp,itemp,lmat(j,natom*3-7), &
+                  lmat(j,natom*3-6)
+            ENDDO
+          ELSE IF(MOD(natom*3-6,5)==3) THEN
+            DO j=1,natom*3
+              READ(79,*) itemp,itemp,itemp,lmat(j,natom*3-8), &
+                  lmat(j,natom*3-7),lmat(j,natom*3-6)
+            ENDDO
+          ELSE IF(MOD(natom*3-6,5)==4) THEN
+            DO j=1,natom*3
+              READ(79,*) itemp,itemp,itemp,lmat(j,natom*3-9), &
+                  lmat(j,natom*3-8),lmat(j,natom*3-7),lmat(j,natom*3-6)
+            ENDDO
+          ENDIF
+        ELSE
+          lblock=(natom*3-6)/5
+          DO i=1,lblock
+            DO k=1,8
+              READ(79,*) buffer
+            END DO
+            DO j=1,natom*3
+              READ(79,*) itemp,itemp,itemp,lmat(j,(i*5-4):(i*5))
+            END DO
           END DO
         END IF
+!      
       ELSE
         WRITE(*,*) "File ",'"',TRIM(filename),'"', " doesn't exist."
       END IF
       CLOSE(79)
 !
-! calculate normal coordinate displacements: dqp !!!!!!!!!!!!!!!!!!!!!!
+! calculate displacement between S0 and S1 states in Cartesian coordinates
+!     
+      dx=0.d0
+      DO i=1,natom
+        dx(i*3-2)=xcoorp(1,i)-xcoor(1,i)
+        dx(i*3-1)=xcoorp(2,i)-xcoor(2,i)
+        dx(i*3)  =xcoorp(3,i)-xcoor(3,i)
+      ENDDO
 !
-      dq=0.d0  ! unit: AMU^(1/2)*Angstrom
-      dqp=0.d0 ! unit: Angstrom
+! calculate normal coordinate displacements dq
+!
+      dq=0.d0
       DO i=1,natom*3-6
         DO j=1,natom*3
-          dqp(i)=dqp(i)+lmatrix(i,j)*dx(1,j)
+          dq(i)=dq(i)+lmat(j,i)*dx(j)
         END DO
-          dq(i)=dqp(i)*DSQRT(redmass(i))    ! unit: AMU^(1/2)*Angstrom
+          dq(i)=dq(i)*DSQRT(redmass(i))    ! unit: AMU^(1/2)*Angstrom
       END DO
 !
       RETURN
--- double_sfg/doubly_sfg.f90
+++ double_sfg/doubly_sfg.f90
@@ -8,58 +8,31 @@
 !                                                                       !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
       PROGRAM dsfg_main
-      USE FC_factor
+      USE constants
+      USE NC_shift 
       IMPLICIT NONE
-      REAL(KIND=8) :: alpha(1:3,1:3),fscale
-      REAL(KIND=8),ALLOCATABLE :: freq(:),dq(:),fc(:,:)
-      INTEGER :: natom,nstate_max,nstate,nu_max
+      REAL(KIND=8) :: omega1,omega2,omega3
+      REAL(KIND=8),ALLOCATABLE :: freq(:),dq(:)
+      INTEGER :: natom
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !      Initialization and Configuration
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!      WRITE(6,*) " The resonant state is (like 1,2,...)"
-!      READ(5,*) i_resonant
-!      WRITE(6,*) " The number of excited states considered:"
-!      READ(5,*) nstate
-!      WRITE(6,*) " The omega_1 and omega_2 (omega_1<omeag_2, eV):"
-!      READ(5,*) omega(1),omega(2)
 !
-
-      natom=10                          ! Total atom number
-      nstate_max=200
-      i_resonant=1                      ! number of resonant state
-      omega(1)=1.55956d0                ! eV
-      omega(2)=3.16834d0                ! eV
-      omega(1)=omega(1)/au2ev           ! au
-      omega(2)=omega(2)/au2ev           ! au
-      omega(3)=omega(1)+omega(2)        ! au
-! files incluing data
-      file_TDM="S0Sn_TEDM_cis_6-31gd.log"
-      file_E="S0Sn_TEDM_cis-d_6-31gd.log"
-!
-! Now calculate polarizability-like terms, alpha !!!!!!!!!!!!!!!!!!!!!!!!
-      OPEN(unit=11,file='alpha.dat',status='unknown')
-      WRITE(11,'(A32)') " nstate   alpha_xy    alpha_yx"
-      DO nstate=0,nstate_max
-        CALL tensor_cal(alpha,nstate)
-        WRITE(11,'(1X,I4,2F18.10)') nstate,alpha(1,2),alpha(2,1)
-      END DO
-      CLOSE(11)
+      natom=64                          ! Total atom number
+      omega1=1.55956d0                  ! eV
+      omega2=3.16834d0                  ! eV
+      omega1=omega1/AU2ev               ! au
+      omega2=omega2/AU2ev               ! au
+      omega3=omega1+omega2              ! au
 !
 ! Now calculate Delta Normal Mode Coordinate !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
       ALLOCATE(freq(natom*3-6))
       ALLOCATE(dq(natom*3-6))
-      CALL Delta_Q(natom,freq,dq) 
-!
-!
-! Now calculate Franck-Condon factor !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!
-      fscale=0.96d0    ! The caled factor to harmonic vibration frequencies
-      nu_max=6         ! The highest value of vibrational quantum number for
-!                      !  excited electronic state.
-      ALLOCATE(fc(0:nu_max,1:natom*3-6))
-      CALL FC_f(natom,freq,dq,fscale,nu_max,fc)
-!      WRITE(*,*) fc(:,2)
+      CALL Delta_Q(natom,dq) 
+      WRITE(*,*) dq
+
 !
       WRITE(*,'(A20)') "THANK GOD! ALL DONE!"
 !
